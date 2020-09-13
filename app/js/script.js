8215243775188443533a2a62ba605335b854c9d9
RyanzpLee <lzpryan@gmail.com>
2020-09-13 13:54:22 +0100

    Navbar function added

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,27 @@
-console.log("Hello world");
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const body = document.querySelector("body");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", function () {
+  console.log("click hamburger");
+
+  if (header.classList.contains("open")) {
+    // Close menu
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach(function (element) {
+      element.classList.add("fade-out");
+      element.classList.remove("fade-in");
+    });
+  } else {
+    // Open menu
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach(function (element) {
+      element.classList.add("fade-in");
+      element.classList.remove("fade-out");
+    });
+  }
+});
